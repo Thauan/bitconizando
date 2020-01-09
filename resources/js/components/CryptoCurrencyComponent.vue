acc90c2464617154ce7fa6bb08efb2bdd3aba239
Thauan <thaubr28@gmail.com>
2020-01-08 23:55:19 -0300

    Carousel select fill crypto watch

--- resources/js/components/CryptoCurrencyComponent.vue
+++ resources/js/components/CryptoCurrencyComponent.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="container-fluid">
-    <div class="row justify-content-center">
+  <div class="container">
+    <carousel :autoplay="true" :items="breakpoints" :nav="false" :dots="true" :loop="true">
+      <div class="cardItem">
+        <div class="currencyItem">
+          <i class="fab fa-3x fa-bitcoin"></i>
+          <h4 class="text-center">Bitcoin</h4>
+        </div>
+      </div>
+    </carousel>
+    <div class="row justify-content-center pt-3">
       <sync-loader :loading="this.loading" :color="this.color" :size="this.size"></sync-loader>
       <div
         class="col-md-12 col-lg-12"
@@ -22,18 +30,24 @@
 <script>
 import axios from "axios";
 import SyncLoader from "vue-spinner/src/PulseLoader.vue";
+import carousel from "vue-owl-carousel2";
 
 export default {
-  components: { SyncLoader },
-  data: () => ({
-    cryptos: [],
-    color: "#fff",
-    size: "30px",
-    loading: true,
-    errors: [],
-    cryptocurrency: ["BTC", "ETH"],
-    currency: ["USD", "EUR"]
-  }),
+  components: { SyncLoader, carousel },
+
+  data() {
+    return {
+      cryptos: [],
+      color: "#fff",
+      size: "30px",
+      screen: {},
+      loading: true,
+      errors: [],
+      cryptocurrency: ["BTC", "ETH"],
+      currency: ["USD", "EUR"],
+      breakpoints: this.sizeRecognition()
+    };
+  },
 
   created() {
     axios
@@ -49,6 +63,35 @@ export default {
         this.loading = false;
         console.log(this.errors);
       });
+
+    this.sizeRecognition();
+  },
+
+  methods: {
+    sizeElement() {
+      this.screen = window.innerWidth;
+      //   console.log("Tamanho da tela " + this.screen + "px");
+      return this.screen;
+    },
+
+    sizeRecognition() {
+      if (this.sizeElement() < 426) {
+        let item = 2;
+        return item;
+      } else if (this.sizeElement() > 426 && this.sizeElement() < 740) {
+        let item = 3;
+        return item;
+      } else if (this.sizeElement() > 768 && this.sizeElement() < 1036) {
+        let item = 6;
+        return item;
+      } else if (this.sizeElement() > 1036 && this.sizeElement() < 1500) {
+        let item = 8;
+        return item;
+      } else {
+        let item = 5;
+        return item;
+      }
+    }
   },
 
   mounted() {
@@ -66,5 +109,22 @@ span.left {
 span.right {
   float: right;
   font-size: 20px;
+}
+
+.currencyItem i {
+  padding: 20px;
+  color: grey;
+}
+
+.currencyItem i:hover {
+  padding: 20px;
+  color: orange;
+}
+
+.cardItem {
+  background: #fff;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 15px;
 }
 </style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,6 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <!-- <div class="card">
-                <div class="card-header">
-                    <h1 class="text-orange"><i class="fab fa-bitcoin"></i> BITCOIN</h1>
-                </div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif -->
-                <crypto-currency></crypto-currency>
-                <!-- </div> -->
-            </div>
-        </div>
-    </div>
+    <crypto-currency></crypto-currency>
 </div>
 @endsection
